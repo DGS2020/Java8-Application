b8776c280cc7fc04b25b589fea56b8b16e8b7a7f
Devanathan-718549 <deva.narasimhan@gmail.com>
2020-04-26 12:43:26 +0530

    Committed Java 1.8 examples in new repository

---
+++ src/com/java8/streams/StreamsApplication.java
@@ -0,0 +1,108 @@
+package com.java8.streams;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class StreamsApplication {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> l = new ArrayList<>();
+		l.add(0);
+		l.add(5);
+		l.add(10);
+		l.add(15);
+		l.add(20);
+		l.add(25);
+		l.add(30);
+		System.out.println("List values " + l);
+		
+		List<Integer> I2 = l.stream().filter(i -> i%2==0).collect(Collectors.toList());
+		
+		System.out.println("Streams filter values " +I2);
+		
+		ArrayList<Integer> marks = new ArrayList<Integer>();
+		marks.add(75);
+		marks.add(45);
+		marks.add(10);
+		marks.add(65);
+		marks.add(20);
+		marks.add(25);
+		marks.add(30);
+		
+		System.out.println("marks value " + marks);
+		
+		long noOfFailedStudents=marks.stream().filter(m->m<35).count();
+		
+		System.out.println("Failed students " + noOfFailedStudents);
+		
+		List<Integer> updatedMarks = marks.stream().map(i-> i+5).collect(Collectors.toList());
+				
+		System.out.println("Updated Marks value " + updatedMarks);
+		
+		Comparator<Integer> com = (i1,i2) -> (i1<i2)?1: (i1>i2)?-1:0;
+		
+		System.out.println("SortedList using comparator " + marks.stream().sorted(com));
+		
+		ArrayList<String> cricketSA = new ArrayList<>();
+		
+		cricketSA.add("Jacquas Kallis");
+		cricketSA.add("Greme Smith");
+		cricketSA.add("Shaun Pollock");
+		cricketSA.add("AB Develliers");
+		cricketSA.add("David Miller");
+		cricketSA.add("Neil Meckenzie");
+		
+		//System.out.println(cricketSA.stream().sorted().collect(Collectors.toList()));
+		
+		//default Natural sorting order
+		System.out.println(cricketSA.stream().sorted((i1,i2)-> i1.compareTo(i2)).collect(Collectors.toList()));
+		System.out.println(cricketSA.stream().sorted((i1,i2)-> -i1.compareTo(i2)).collect(Collectors.toList()));
+		//Logic here is String length we have to compare and print the values
+		Comparator<String> comp = (s1,s2) -> {
+			int l1=s1.length();
+			int l2=s2.length();
+			if(l1 < l2) return -1;
+			else if(l1 > l2) return +1;
+			else return s1.compareTo(s2);
+		};
+		
+		List<String> outputCricketPlayers = cricketSA.stream().sorted(comp).collect(Collectors.toList());
+		System.out.println("Outputcricket palyers with length " +  outputCricketPlayers);
+		
+		
+		//Min max using streams examples.
+		//Natural sorting ascending order
+		ArrayList<Integer> l1 = new ArrayList<Integer>();
+		l1.add(10);
+		l1.add(0);
+		l1.add(15);
+		l1.add(5);
+		l1.add(20);
+		System.out.println("List values for Min/Max using streams " + l1);
+		Integer min = l1.stream().min((i1,i2)-> i1.compareTo(i2)).get();
+		System.out.println("min value " + min);
+		
+		Integer max = l1.stream().max((i1,i2)-> i1.compareTo(i2)).get();
+		System.out.println("max value " + max);
+		
+		//reverse order of sroting i.e descending order
+		
+		Integer min1 = l1.stream().min((i1,i2)-> -i1.compareTo(i2)).get();
+		System.out.println("reverse min value " + min1);
+		
+		Integer max1 = l1.stream().max((i1,i2)-> -i1.compareTo(i2)).get();
+		System.out.println("reverse max value " + max1);
+		
+		//in Stream we have forEach will return Function, in java 1.8 not required to use iterator to print the collections value, instead of that we can use Method reference.
+		Consumer<Integer> f= i->{
+			System.out.println("The Square of " + i + "is: " + (i*i));
+		};
+		
+		l1.stream().forEach(f);
+		
+	}
+
+}
